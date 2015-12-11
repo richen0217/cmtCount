33cb0b53b840607ddae91a7ace4b5db1547498fb
richen <sc108201409@163.com>
2015-12-11 17:58:34 +0800

    1211

---
+++ WinComment/FormTotal.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WinComment
+{
+    partial class FormTotal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGet = new System.Windows.Forms.Button();
+            this.GVTotal = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.GVTotal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(311, 13);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 0;
+            this.btnGet.Text = "获得";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // GVTotal
+            // 
+            this.GVTotal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GVTotal.Location = new System.Drawing.Point(27, 48);
+            this.GVTotal.Name = "GVTotal";
+            this.GVTotal.RowTemplate.Height = 23;
+            this.GVTotal.Size = new System.Drawing.Size(804, 306);
+            this.GVTotal.TabIndex = 1;
+            // 
+            // FormTotal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 366);
+            this.Controls.Add(this.GVTotal);
+            this.Controls.Add(this.btnGet);
+            this.Name = "FormTotal";
+            this.Text = "FormTotal";
+            ((System.ComponentModel.ISupportInitialize)(this.GVTotal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.DataGridView GVTotal;
+    }
+}
---
+++ WinComment/FormTotal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinComment
+{
+    public partial class FormTotal : Form
+    {
+        public FormTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
